d1780681bba39f1a908e237434f32a533ca41d24
eduardoevaldt <eduardo.webber@rede.ulbra.br>
2021-09-08 21:54:50 -0300

    feat: Adicionado rotas de editar categoria

--- src/category/category.controller.ts
+++ src/category/category.controller.ts
@@ -2,19 +2,27 @@ import { Body, Controller, Get, Param, Post, Put } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { CreateCategoryDto } from './dto/create-category.dto';
+import { UpdateCategotyDto } from './dto/update-category.dto';
 import { Category, CategoryDocument } from './schemas/category.schema';
 
 @Controller('api/category')
 export class CategoryController {
   constructor(
-    @InjectModel(Category.name) private eventModel: Model<CategoryDocument>,
+    @InjectModel(Category.name) private categoryModel: Model<CategoryDocument>,
   ) {}
 
   @Get() public getAll() {
-    return this.eventModel.find().exec();
+    return this.categoryModel.find().exec();
   }
 
   @Post() public async create(@Body() createCategoryDto: CreateCategoryDto) {
-    await this.eventModel.create(createCategoryDto);
+    await this.categoryModel.create(createCategoryDto);
+  }
+  
+  @Put(':id') public async update(
+    @Param('id') _id: number,
+    @Body() updateCategoryDto: UpdateCategotyDto,
+  ) {
+    await this.categoryModel.updateOne({ _id }, updateCategoryDto);
   }
 }
